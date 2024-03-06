342fc0ebdf16c5f1289ee0fda7c36002b449ccb0
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-03-06 10:44:08 +0800

    0306

--- onetap-compose/src/main/java/com/vk/id/onetap/compose/button/auth/style/VKIDButtonRippleStyle.kt
+++ onetap-compose/src/main/java/com/vk/id/onetap/compose/button/auth/style/VKIDButtonRippleStyle.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Color
 import com.vk.id.common.InternalVKIDApi
 import com.vk.id.onetap.common.auth.style.VKIDButtonRippleStyle
 
+@OptIn(InternalVKIDApi::class)
 internal fun VKIDButtonRippleStyle.asColor() = when (this) {
     VKIDButtonRippleStyle.DARK -> Color.Black
     VKIDButtonRippleStyle.LIGHT -> Color.White
