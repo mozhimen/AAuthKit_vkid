d9b635aafe64c68da0de8899d724ee93336a6a51
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-03-07 17:43:04 +0800

    0307

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AAuthKit_vkid
 Android校验库 -> vkid
+```groovy
+//AAuthKit_vkid
+include ':AAuthKit_vkid'
+project(':AAuthKit_vkid').projectDir = new File("D:\\WorkSpace\\GitHub\\AAuthKit_vkid")
+include ':AAuthKit_vkid:authk_vkid'
+```
--- app/src/main/java/com/mozhimen/authk/vkid/test/MainActivity.kt
+++ app/src/main/java/com/mozhimen/authk/vkid/test/MainActivity.kt
@@ -2,10 +2,25 @@ package com.mozhimen.authk.vkid.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mozhimen.authk.vkid.AuthKVKIDOnTapBottomSheet
+import com.mozhimen.authk.vkid.optins.OBuildApp_NeedManifestPlaceholders
+import com.mozhimen.authk.vkid.optins.OBuildApp_NeedSecretsProperties
+import com.mozhimen.authk.vkid.test.databinding.ActivityMainBinding
+import com.mozhimen.basick.elemk.androidx.appcompat.bases.viewbinding.BaseActivityVB
+import com.mozhimen.basick.lintk.optins.OApiCall_BindLifecycle
+import com.mozhimen.basick.lintk.optins.OApiCall_BindViewLifecycle
+import com.mozhimen.basick.lintk.optins.OApiInit_ByLazy
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivityVB<ActivityMainBinding>() {
+    @OptIn(OApiInit_ByLazy::class, OApiCall_BindLifecycle::class, OApiCall_BindViewLifecycle::class, OBuildApp_NeedSecretsProperties::class, OBuildApp_NeedManifestPlaceholders::class)
+    private val _authKVkidProxy: AuthKVKIDOnTapBottomSheet<MainActivity> by lazy { AuthKVKIDOnTapBottomSheet(this) }
+
+    @OptIn(OApiInit_ByLazy::class, OApiCall_BindLifecycle::class, OApiCall_BindViewLifecycle::class, OBuildApp_NeedSecretsProperties::class, OBuildApp_NeedManifestPlaceholders::class)
+    override fun initView(savedInstanceState: Bundle?) {
+        _authKVkidProxy.apply {
+            bindLifecycle(this@MainActivity)
+
+        }
     }
 }
